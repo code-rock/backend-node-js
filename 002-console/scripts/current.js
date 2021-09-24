20fcff5033c6ddc43bdb6ee38feb829fb5802671
code-rock <bind.client.illumination@yandex.ru>
2021-09-24 09:34:26 +0300

    Fix issues

--- 002-console/scripts/current.js
+++ 002-console/scripts/current.js
@@ -1,10 +1,28 @@
 #!/usr/bin/env node
 
 const { argv } = require("yargs");
-const { year, y, month, m, date, d } = argv;
+const { year, y, month, m, date, d, _ } = argv;
 const curr = new Date();
+const isSub = _.includes('sub');
+const isAdd = _.includes('add');
 
-if (year || y) {
+const change = (curr) => {
+    const sign = isSub ? -1 : 1
+
+    if (year || y) {
+        curr.setFullYear(curr.getFullYear() + sign * (year || y));
+    } else if (month || m) {
+        curr.setMonth(curr.getMonth() + sign * (month || m));
+    } else if (date || d) {
+        curr.setDate(curr.getDate() + sign * (date || d));
+    }
+
+    return new Date(curr);
+}
+
+if (isAdd || isSub) { 
+    console.log('Текущая дата и время в формате ISO: ', change(curr).toISOString());
+} else if (year || y) {
     console.log('Текущий год: ', curr.getFullYear());
 } else if (month || m) {
     console.log('Текущий месяц: ', curr.getMonth());
