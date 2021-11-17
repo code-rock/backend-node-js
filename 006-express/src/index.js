808a8bc4c98ff3335ee7e003bf6de57eec166ed9
code-rock <bind.client.illumination@yandex.ru>
2021-11-17 14:45:06 +0300

    Something with ports

--- 006-express/src/index.js
+++ 006-express/src/index.js
@@ -26,6 +26,6 @@ app.use('/api/books', booksRouter);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, '0.0.0.0');
+
+console.log(`Сервер слушает порт: ${PORT}`);
