477edbff98c5fe5753c9452c85567d600e6366a4
code-rock <bind.client.illumination@yandex.ru>
2021-12-04 17:44:14 +0300

    fix static ways

--- 011-mongo/mongoose/src/web/app.ts
+++ 011-mongo/mongoose/src/web/app.ts
@@ -44,11 +44,11 @@ const server = http.Server(app);
 // @ts-ignore
 const io = socketIO(server);
 
-app.set('views', __dirname + '/views');
+app.set('views', process.env.WORKING_DIR + '/views');
 app.use(loggerMiddleware);
 app.set('view engine', 'ejs');
 
-app.use('public', express.static(__dirname + "./public"));
+app.use('public', express.static(process.env.WORKING_DIR + "/public"));
 app.use(require('express-session')({
     secret: process.env.COOKIE_SECRET,
     resave: false,
